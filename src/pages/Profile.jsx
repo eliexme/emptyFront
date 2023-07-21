051189fc1d04abfde0fd7ca21392131ef6620903
Elisa Exposito <eliexme@gmail.com>
2023-07-22 00:24:16 +0200

    create workout in profile page done. Connection to backend pending with repes, series and weight

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,98 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/auth.context';
 
 export default function Profile() {
     const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+    const [showpop, setShowpop] = useState(false)
+    const [allexercises, setAllexercises] = useState([])
+    const [newrutina, setNewrutina] = useState([])
+    const [rutinadata, setRutinadata] = useState([])
+
+    const toggleRutina = ()=>{
+        setShowpop(!showpop)
+    }
+
+    const fetchExercises = async()=>{
+        const response = await fetch('http://localhost:5005/exercise')
+        if(response.status === 200 || response.status === 304){
+            const parsed = await response.json()
+            setAllexercises(parsed)
+        }
+    }
+
+    const addToRutina = (exerId) => {
+        if (!newrutina.includes(exerId)) {
+            setNewrutina((prevRutina) => {
+                const updatedRutina = [...prevRutina, exerId];
+                setRutinadata(allexercises.filter((eachExer) => updatedRutina.includes(eachExer._id)));
+                return updatedRutina;
+            });
+        }
+    };
+
+    useEffect(()=>{
+        fetchExercises()
+    },[])
+
+    useEffect(()=>{
+        console.log(rutinadata)
+    },[rutinadata])
+
+
 
     return (
         <div>
             {user ? (
-                <div>Hello to profile {user.name}</div>
+                <div>
+                    <h2>Hello to profile {user.name}</h2>
+                    <button onClick={toggleRutina}>Añadir rutina</button>
+
+                    <div className={`${showpop ? 'showpop' : 'none'}`}>
+                        <button onClick={toggleRutina}>X</button>
+
+                        <div className='grid'>
+                            {allexercises ? 
+                                allexercises.map((oneExer)=>(
+                                    <div key={oneExer._id}>
+                                        <img onClick={()=>addToRutina(oneExer._id)} className='thumb' src={oneExer.imagen}/>
+                                        <h4>{oneExer.nombre}</h4>
+                                    </div>
+                                ))
+                            : <p>Loading...</p>}
+                        </div>
+
+                        <div>
+                            <h2>Tu rutina</h2>
+                            <div className='grid'>
+                                {rutinadata ? 
+                                    rutinadata.map((oneExer)=>(
+                                        <div key={oneExer._id}>
+                                            <img onClick={()=>addToRutina(oneExer._id)} className='thumb' src={oneExer.imagen}/>
+                                            <h4>{oneExer.nombre}</h4>
+                                            <form>
+                                                <div>
+                                                    <label>Series</label>
+                                                    <input type='number'/>
+                                                </div>
+
+                                                <div>
+                                                    <label>Repes</label>
+                                                    <input type='number'/>
+                                                </div>
+
+                                                <div>
+                                                    <label>Peso</label>
+                                                    <input type='number'/>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    ))
+                                : <p>Loading...</p>}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             ) : (
                 <p>Loading...</p>
             )}
